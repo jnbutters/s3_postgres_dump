7f00832c7177c3dab51d10e9a4b0fcacd519c20b
Jason Butters <jason.butters@gov.scot>
2023-07-27 09:02:21 +0100

    add s3 objects list

---
+++ s3_list_objects.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+# Set current directory
+DIR=`pwd`
+
+# Import config file
+source $DIR/config
+
+# Vars
+NOW=$(date +"%Y-%m-%d-at-%H-%M-%S")
+
+# List S3 Objects
+aws s3api list-objects --bucket $S3_BUCKET --query 'Contents[].{Key: Key, Size: Size}'
+
